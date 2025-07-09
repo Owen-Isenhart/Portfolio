50596cfbf146debdac48f40f27c04d68bd81fae0
Owen Isenhart <isenhart.owen@gmail.com>
2025-07-09 16:55:59 -0500

    attempting to fix notion image expiration

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/secure.notion-static.com/**',
+      },
     ],
   },
 };
